1e8b080d123f4fcce12c0444e14f98db26d6f4c0
Andres Leonardo <andresleonardobg@gmail.com>
2024-08-03 22:01:41 -0500

    feat(checkbox): :sparkles: new component: checkbox

---
+++ components/checkbox.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+
+export function CheckBox({ name, selected, setSelected }) {
+  let checked;
+
+  if (selected === name) {
+    checked = <Text>X</Text>;
+  }
+
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View>
+        <TouchableOpacity
+          onPress={() => setSelected(name)}
+          style={{
+            borderWidth: 2,
+            borderColor: "black",
+            width: 30,
+            height: 30,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {checked}
+        </TouchableOpacity>
+      </View>
+      <Text>{name}</Text>
+    </View>
+  );
+}
