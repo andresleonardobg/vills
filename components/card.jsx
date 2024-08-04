9b2b8fa3d629e812b2a2ce4319a30fbb76b90022
Andres Leonardo <andresleonardobg@gmail.com>
2024-08-03 20:16:49 -0500

    refactor(card): :art: restructure it

--- components/card.jsx
+++ components/card.jsx
@@ -3,9 +3,9 @@ import { StyleSheet, View, Text } from "react-native";
 export function Card({ input }) {
   return (
     <>
-      <View style={input ? styles.input : styles.output}>
-        <Text>Comida</Text>
-        <Text>-$100000</Text>
+      <View style={input.typeRecord ? styles.input : styles.output}>
+        <Text>{input.name}</Text>
+        <Text>{input.value}</Text>
       </View>
     </>
   );
@@ -14,14 +14,12 @@ export function Card({ input }) {
 const styles = StyleSheet.create({
   output: {
     backgroundColor: "yellow",
-    width: "90%",
     padding: 20,
     borderWidth: 5,
     borderRadius: 20,
   },
   input: {
     backgroundColor: "green",
-    width: "90%",
     padding: 20,
     borderWidth: 5,
     borderRadius: 20,
