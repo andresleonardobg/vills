a0defceb56436974406413ef3eaf8bfa7c5efc5b
Andres Leonardo <andresleonardobg@gmail.com>
2024-08-11 16:59:53 -0500

    feat(types): :art: types
    files to store types

---
+++ types/types.ts
@@ -0,0 +1,18 @@
+export type Transaction = {
+  id: Promise<number>;
+  type: boolean;
+  amount: number;
+  category: string;
+  year: number;
+  month: string;
+  day: number;
+  hour: (number | string)[];
+  description: string;
+};
+
+export type propsTransaction = {
+  modalVisible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  transactions: Transaction[];
+  setData: React.Dispatch<React.SetStateAction<Transaction[]>>;
+};
