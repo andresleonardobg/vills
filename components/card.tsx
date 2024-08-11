d45049b0d4b7d925e8429e9d91692737dd532471
Andres Leonardo <andresleonardobg@gmail.com>
2024-08-10 22:11:41 -0500

    refactor(card): :art: setting data

--- components/card.tsx
+++ components/card.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, View, Text } from "react-native";
 
-export function Card({ input }) {
+export function Card({ data }) {
   return (
     <>
-      <View style={input.type ? styles.input : styles.output}>
-        <Text>{input.category}</Text>
-        <Text>{input.amount}</Text>
+      <View style={data.type ? styles.input : styles.output}>
+        <Text>{data.category}</Text>
+        <Text>{data.amount}</Text>
       </View>
     </>
   );
